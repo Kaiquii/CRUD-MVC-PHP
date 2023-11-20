8b93f60ed566cc0e02df0f5d2541daf49ed37b1f
Kaiquii <112667885+Kaiquii@users.noreply.github.com>
2023-11-20 12:37:46 -0300

    Add files via upload

---
+++ Projeto CRUD em MVC/src/views/partials/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css" />
+    <title>Meu CRUD em MVC</title>
+</head>
+<body>
+<header>
+    <h1>Título do site</h1>
+    <hr>
+</header>
+
+
